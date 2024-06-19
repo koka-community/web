b596970b4572af01bc1adb3da2c28b31453e030b
Tim Whiting <tim@whitings.org>
2024-06-19 08:37:24 -0600

    reorganize

--- generator/ext/js_core.mjs
+++
@@ -1,9 +0,0 @@
-
-function maybeget(obj, key) {
-  const _x = obj[key];
-  if (_x === undefined || _x === null) {
-    return $std_core_types.Nothing;
-  } else {
-    return $std_core_types.Just(_x);
-  }
-}
