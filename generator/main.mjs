a5b8ce6c714fe6a74380996f05fe35b7f18dd766
Tim Whiting <tim@whitings.org>
2024-06-24 23:41:12 -0600

    clean up old changes

---
+++ generator/main.mjs
@@ -0,0 +1,28 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import * as childProcess from 'child_process';
+import * as fs from 'fs';
+import * as css from '@webref/css';
+import * as elements from '@webref/elements';
+import * as idl from '@webref/idl';
+
+// Setup properties for JS interop in Dart.
+globalThis.self = globalThis;
+globalThis.childProcess = childProcess;
+globalThis.css = css;
+globalThis.elements = elements;
+globalThis.fs = fs;
+globalThis.idl = idl;
+globalThis.location = { href: `file://${process.cwd()}/` }
+
+globalThis.setup = async function () {
+  globalThis.cssSync = await css.listAll()
+  globalThis.elementsSync = await elements.listAll()
+  globalThis.idlSync = await idl.parseAll();
+}
+
+globalThis.doSetup = function(f) {
+  globalThis.setup().then(f);
+}
